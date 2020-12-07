dfe5bd49180171e18a4f4264534228e85d54ece1
Meiyi <hemeiyi0225@gmail.com>
2020-12-06 16:26:45 -0800

    Merge Sorter and Quick Sorter

--- src/_00_Sorting_Algorithms/MergeSorter.java
+++ src/_00_Sorting_Algorithms/MergeSorter.java
@@ -13,54 +13,74 @@ public class MergeSorter extends Sorter {
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 		//20. call the mergeSort method with 0 and the length of the array minus one
+		mergeSort(array,0,array.length,display);
 	}
 	
 	private void mergeSort(int[] array, int low, int high, SortingVisualizer display) {
 		//1. Create a temporary integer array that is the same length as the passed in array.
-		
+		int [] arr = new int [array.length] ;
 		//2. make an if statement that checks if low is less than high
 		//   and put the rest of the method inside of it
-
+		if (low<high) {
+	
+		
 			//3. Create an integer called middle and set it 
 			//   equal to the half way point between low and high
-            
+            int middle = 1/2*(high-low);
             //4. call the mergeSort method with low and middle
-           
+           mergeSort(array, low, middle, display);
             //5. call the mergeSort method with middle + 1 and high
-            
+            mergeSort(array,middle+1,high, display);
             //6. copy the elements from the array into the temporary array,
             //   but only the elements from low to high inclusive
-            
+            for (int i=0;i<array.length;i++) {
+            	
+            }
             //7. create three integers called i, j, and k and
             //   set them equal to low, middle + 1, and low respectively
-            
+            int i=low;
+            int j=middle+1;
+            int k=low;
             //8. while i is less than or equal to middle
             //   and j is less than or equal to high
 	    // (ends after step 16)
+            while (i<=middle&&j<=high) {
+            	
             
             	//9. if temp array at i is less than or equal 
             	//   to temp array at j	
-                
+               if ( arr[i]<=arr[j]) {
+            	   array[k]=arr[i];
+            	   i++;
+               }
                     //10. set array at k equal to temp array at i
                     
                     //11. increase i by 1
                   
                 //13. else (ends after step 15)
-            
+               else {
+            	   
+               
                     //14. set array at k equal to temp array at j
-                   
+                   array[k]=arr[j];
                     //15. increase j by 1
-                 
+                   j++;
+               }
                 //16. increase k by 1
-                
-            
+                k++;
+            }
             //17. make a while loop that runs while i is less than or equal to middle
+            while (i<=middle) {
+            	array[k]=arr[i];
+            	k++;
+            	i++;
+            }
             
             	//18. set array at k equal to temp array at i
                 
                 //19. increase k and i by 1
-               
-            
+	        
+		}
         
 	}
 
