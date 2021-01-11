fbe7b778f3faa236d266f438237639dfd17e40d2
Meiyi <hemeiyi0225@gmail.com>
2021-01-10 16:29:33 -0800

    Searching algorithms almost finished

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -1,28 +1,28 @@
 package _00_Sorting_Algorithms;
 
-public class BubbleSorter extends Sorter{
+public class BubbleSorter extends Sorter {
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
-	//1. Use the bubble sorting algorithm to sort the array.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// 1. Use the bubble sorting algorithm to sort the array.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		boolean isorder=false;
-		while (isorder==false) {
-			isorder=true;
-		for (int i=0;i<array.length;i++) {
-			if (array[i]<array[i-1]) {
-				int num=array[i];
-				array[i]=array[i-1];
-				array[i-1]=num;
-				isorder=false;
+		boolean isorder = false;
+		while (isorder == false) {
+			isorder = true;
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] < array[i - 1]) {
+					int num = array[i];
+					array[i] = array[i - 1];
+					array[i - 1] = num;
+					isorder = false;
+				}
 			}
 		}
-	}
 		display.updateDisplay();
 	}
-	
+
 }
--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -5,55 +5,85 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		for (int i=0;i<eggs.size();i++) {
+		for (int i = 0; i < eggs.size(); i++) {
 			if (eggs.get(i).equals("cracked")) {
 				return i;
 			}
 		}
 		return -1;
 	}
+
 	public static int countPearls(List<Boolean> oysters) {
-		for (int i=0;i<oysters.size();i++) {
-			if (oysters.get(i)==true) {
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
 				return i;
 			}
 		}
 		return -1;
 	}
+
 	public static double findTallest(List<Double> peeps) {
-		double big=Integer.MIN_VALUE;
-		for (int i=0;i<peeps.size();i++) {
-			if (peeps.get(i)>big) {
-				big=peeps.get(i);
-			
+		double big = Integer.MIN_VALUE;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > big) {
+				big = peeps.get(i);
+
 			}
 		}
 		return big;
 	}
+
 	public static String findLongestWord(List<String> w) {
 		int longest = 0;
 		String l = "";
-		for (int i =0;i<w.size();i++) {
-			if (w.get(i).length()>longest) {
-				longest =w.get(i).length();
+		for (int i = 0; i < w.size(); i++) {
+			if (w.get(i).length() > longest) {
+				longest = w.get(i).length();
 				l = w.get(i);
 			}
 		}
 		return l;
 	}
-	
-	public static boolean containsSOS (List<String> m) {
+
+	public static boolean containsSOS(List<String> m) {
 		if (m.contains("... --- ...")) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
-	public static List<Double> sortScores (List<Double> results) {
-		List<Double> out= new ArrayList<Double>();
-		return out;
+
+	public static List<Double> sortScores(List<Double> results) {
+		boolean isorder = false;
+		while (isorder == false) {
+			isorder=true;
+		for (int i = 1; i < results.size(); i++) {
+			if (results.get(i) < results.get(i - 1)) {
+				double num = results.get(i);
+				results.set(i, results.get(i - 1));
+				results.set(i - 1, num);
+				isorder=false;
+			}
+
+		}
+		}
+		return results;
 	}
 	
+	public static List<String> sortDNA (List<String> sortedSequences) {
+		boolean isorder = false;
+		while (isorder == false) {
+			isorder=true;
+		for (int i = 1; i < sortedSequences.size(); i++) {
+			if (sortedSequences.get(i).length() < sortedSequences.get(i - 1).length()) {
+				String num = sortedSequences.get(i);
+				sortedSequences.set(i, sortedSequences.get(i - 1));
+				sortedSequences.set(i - 1, num);
+				isorder=false;
+			}
+
+		}
+		}
+		return sortedSequences;
+	}
 }
