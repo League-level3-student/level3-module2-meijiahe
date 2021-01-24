f3ce29b700b40bfec5624f58995b5d36c5689c6a
Meiyi <hemeiyi0225@gmail.com>
2021-01-24 15:20:00 -0800

    String Trimmer (teacher said move on)

--- src/_99_extra/recursion_practice/StringTrimmer.java
+++ src/_99_extra/recursion_practice/StringTrimmer.java
@@ -2,21 +2,26 @@ package _99_extra.recursion_practice;
 
 import static org.junit.Assert.assertEquals;
 
-public class StringTrimmer {
+//public class StringTrimmer {
 	
-	static public String trimString(String str, int removesLeft) {
+	//static public String trimString(String str, int removesLeft) {
 		//1. If there are no removes left
 			//2.  return the string
-		
-		//3. Else you need to substring the string by 1, reduce removesLeft by 1, and then return both. 
-		
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		assertEquals("Hello W", trimString("Hello World!", 5));
-		assertEquals("League", trimString("LeagueOfAmazing!", 10));
-		assertEquals("James", trimString("JamesTheIntern", 9));
-		assertEquals("", trimString("TheWholeWord", 12));
-	}
-}
+//		if (removesLeft==0) {
+//			return str;
+//		}
+//		//3. Else you need to substring the string by 1, reduce removesLeft by 1, and then return both. 
+//		else {
+//			str.substring(1);
+//			removesLeft--;
+//		}
+//		return str, removesLeft;
+////	}
+//	
+//	public static void main(String[] args) {
+//		assertEquals("Hello W", trimString("Hello World!", 5));
+//		assertEquals("League", trimString("LeagueOfAmazing!", 10));
+//		assertEquals("James", trimString("JamesTheIntern", 9));
+//		assertEquals("", trimString("TheWholeWord", 12));
+//	}
+//}
